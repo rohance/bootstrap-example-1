ec37cbdae4e56ad532efb97e8623d5bfa5b92e71
rohance <mycamera483@gmail.com>
2022-06-27 14:24:04 +0600

    adding css file

---
+++ New folder (6)/calculate.js
@@ -0,0 +1,13 @@
+var num1 = 10;
+var num2 = 20;
+var num3 = 50;
+
+var avg = ( num1+num2+num3)/3;
+
+document. getElementById('box2').innerText = "average of all 3 numbers are: " + avg;
+
+var Studentname = "abc";
+
+var msg = "Nice to know you , our new stuedent: " + Studentname;
+
+document.getElementById('box1').innerText = msg;
