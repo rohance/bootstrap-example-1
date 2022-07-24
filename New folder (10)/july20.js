012b91c34c24310c02456fa88e35d16fe4448b19
rohance <mycamera483@gmail.com>
2022-07-24 09:56:09 +0600

    adding css file

---
+++ New folder (10)/july20.js
@@ -0,0 +1,34 @@
+// var quiz = [10, 0, 9, 7, 33, 45, 67, 35, 43, 65, 89]
+
+
+// console.log(quiz[0])
+
+// console.log("total no of items in this array: ", quiz.length)
+
+
+// console.log(quiz)
+
+// var student1 = {
+//     name: "Mr. Covid", major: "Eng", cgpa: 3.44, phNo: 14567938439, bGr: "A+", address: "Banani", email: "abc@xyz.com"
+// }
+
+// var products = [
+//     { item: "phone", price: 20000, color: "grey", pYr: 2021 },
+// { item: "laptop", price: 74000, color: "black", pYr: 2022 },
+// { item: "printer", price: 15000, color: "white", pYr: 2015 },
+
+// ]
+
+
+// console.log(products[1])
+
+
+var number;
+
+for (number = 10; number <= 50; number+=5) {
+
+    console.log(number)
+
+}
+
+
