36fb35787b97ccf637d2ea8dfcb44a921c6aeb0f
rohance <mycamera483@gmail.com>
2022-08-14 08:11:45 +0600

    adding css file

--- assignment/4/app.js
+++ assignment/4/app.js
@@ -1,48 +1,51 @@
 
 
-var text = '';
-function findCountry(){
+// var text = '';
+function findCountry() {
     text = document.getElementById('country-search').value;
     connectCountry(text);
 
+    
 }
 
 
 function connectCountry(text) {
-    fetch(`https://api.covid19api.com/total/dayone/country/${text}`)
-    .then(res=> res.json() )
-    .then(cdata=> loadcountry(cdata));
+    fetch(`https://api.covid19api.com/live/country/${text}`)
+        .then(res => res.json())
+        .then(cdata => loadcountry(cdata));
+
+
 
 }
 
 connectCountry();
 
-function loadcountry(cdata){
+function loadcountry(cdata) {
     var countryCoronraInfo = document.getElementById('corona-info-of-a-country');
     countryCoronraInfo.innerHTML = `
-                                    <h1> ${cdata[cdata.length-1].Active}</h1>
+                                    <h1> ${cdata[cdata.length - 1].Active}</h1>
                                     <p>Active Cases</p>
-                                    <h1>${cdata[cdata.length-1].Confirmed}</h1>
+                                    <h1>${cdata[cdata.length - 1].Confirmed}</h1>
                                     <p>Confirmed Cases</p>
-                                    <h1>${cdata[cdata.length-1].Deaths}</h1>
+                                    <h1>${cdata[cdata.length - 1].Deaths}</h1>
                                     <p>Death Cases</p>
-                                    <p>Date: ${cdata[cdata.length-1].Date}</p>
+                                    <p>Date: ${cdata[cdata.length - 1].Date}</p>
                                     <button onclick="FindCountryDetails()">More Details</button><br> <br>
                                     `;
 }
 
-function FindCountryDetails(){
+function FindCountryDetails() {
     text = document.getElementById('country-search').value;
     connectCountryDetails(text);
 }
 
-function connectCountryDetails(text){
+function connectCountryDetails(text) {
     fetch(`https://restcountries.com/v3.1/name/${text}`)
-    .then(res=> res.json() )
-    .then(CountryDetailsdata=> LoadCountryDetails(CountryDetailsdata));
+        .then(res => res.json())
+        .then(CountryDetailsdata => LoadCountryDetails(CountryDetailsdata));
 }
 
-function LoadCountryDetails(CountryDetailsdata){
+function LoadCountryDetails(CountryDetailsdata) {
     console.log(CountryDetailsdata);
     var countryDetailsDiv = document.getElementById('countrydetails');
     countryDetailsDiv.innerHTML = ` <img src='${CountryDetailsdata[0].flags.png}'><br>
@@ -52,5 +55,5 @@ function LoadCountryDetails(CountryDetailsdata){
                                     <p>Capital: ${CountryDetailsdata[0].capital[0]}<p>
                                     <p>Region: ${CountryDetailsdata[0].region}<p>
                                     `;
-    
+
 }
